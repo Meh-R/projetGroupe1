711cfd72fd1a6cbdb5146aa26e7266d47a72024a
mehdi <rabellemehdi@gmail.com>
2024-03-06 14:44:35 +0100

    finish

--- js/js.js
+++ js/js.js
@@ -1,8 +1,17 @@
 function changeBackgroundColor() {
   let body = document.querySelector(".body");
-
+  let footer = document.querySelector("footer");
+  let fourtContainer = document.querySelector(".fourth-container");
   body.classList.toggle("bg-black");
+  footer.classList.toggle("bg-black");
+  fourtContainer.classList.toggle("bg-black");
+}
+
+function changeBackgroundColorHeader() {
+  let header = document.querySelector(".header");
+  header.classList.toggle("hd-black");
 }
 
 let button2 = document.querySelector(".header-container-button");
 button2.addEventListener("click", changeBackgroundColor);
+button2.addEventListener("click", changeBackgroundColorHeader);
